2b4e39096f1ce49f2ee548ce96fc106be685f094
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2023-08-05 13:31:10 +0530

    Add printBooks() func to display books in DOM.

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const inputAuthor = document.getElementById("author");
 const inputPages = document.getElementById("pages");
 const inputIsRead = document.getElementById("isRead");
 const inputBookForm = document.getElementById("newBookForm");
+const booksOutput = document.getElementById("books");
 
 let myLibrary = [];
 
@@ -23,8 +24,7 @@ const addBook = () => {
     inputIsRead.checked
   );
   myLibrary.push(newBook);
-  console.log(newBook);
-  console.log(myLibrary);
+  printBooks();
 };
 
 inputBookForm.addEventListener("submit", (event) => {
@@ -32,16 +32,36 @@ inputBookForm.addEventListener("submit", (event) => {
   addBook();
 });
 
-//   ~@@@@@@P        !@&:    B@@@@@#.
-//   ~@@@@@@P        !@&:    B@@@@@#.
-//   ~@@@@@@P        !@&:    B@@@@@#.
-//   ~@@@@@@P        !@&:    B@@@@@#.
-//   ~@@@@@@P        !@&:    B@@@@@#.
-//   ~@@@@@@P        !@&:    B@@@@@#.
-//   ~@@@@@@P        !@&:    B@@@@@#.
-//   ~@@@@@@P        !@&:    B@@@@@#.
-//   ~@@@@@@P        !@&:    B@@@@@#.
-//   ~@@@@@@P        !@&.    B@@@@@#.
-//   .B@@@@@&^       5@G     B@@@@@#.
-//    :P@@@@@&Y7~~!?B@G:     B@@@@@#.
-//      ^?5G#&&@&##GJ~       G@@@@@#.
+//   ~@@@@@P        !@@&:    B@@@@#.
+//   ~@@@@@P        !@@&:    B@@@@#.
+//   ~@@@@@P        !@@&:    B@@@@#.
+//   ~@@@@@P        !@@&:    B@@@@#.
+//   ~@@@@@P        !@@&:    B@@@@#.
+//   ~@@@@@P        !@@&:    B@@@@#.
+//   ~@@@@@P        !@@&.    B@@@@#.
+//   .B@@@@&^       5@@G     B@@@@#.
+//    :P@@@@&Y7~~!?B@@G:     B@@@@#.
+//      ^?5G#&&@&##GJ~       G@@@@#.
+
+const printBooks = () => {
+  if (myLibrary.length === 0) {
+    let p = document.createElement("p");
+    p.textContent = "Please add book(s)";
+    p.id = "noBook";
+    booksOutput.appendChild(p);
+  } else {
+    let noBookP = document.getElementById("noBook");
+    if (booksOutput.contains(noBookP)) {
+      booksOutput.removeChild(noBookP)
+    }
+    for (let i = 0; i < myLibrary.length; i++) {
+      let object = myLibrary[i];
+      console.log(object)
+      let bookInfo = document.createElement("p"); // Temporary
+      bookInfo.textContent = object.title + " by " + object.author + " having " + object.pages + " pages.";
+      booksOutput.appendChild(bookInfo);
+    }
+  }
+};
+
+printBooks();

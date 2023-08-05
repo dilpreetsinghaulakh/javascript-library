f032c178aa657ab2e063612d8fb584803173338b
Dilpreet Singh <dilpreetsinghaulakh@icloud.com>
2023-08-05 15:51:16 +0530

    Fix repitition of books when adding. [printBooks()]

--- script.js
+++ script.js
@@ -44,22 +44,33 @@ inputBookForm.addEventListener("submit", (event) => {
 //      ^?5G#&&@&##GJ~       G@@@@#.
 
 const printBooks = () => {
+  booksOutput.replaceChildren();
   if (myLibrary.length === 0) {
     let p = document.createElement("p");
     p.textContent = "Please add book(s)";
-    p.id = "noBook";
     booksOutput.appendChild(p);
   } else {
-    let noBookP = document.getElementById("noBook");
-    if (booksOutput.contains(noBookP)) {
-      booksOutput.removeChild(noBookP)
-    }
     for (let i = 0; i < myLibrary.length; i++) {
       let object = myLibrary[i];
-      console.log(object)
       let bookInfo = document.createElement("p"); // Temporary
-      bookInfo.textContent = object.title + " by " + object.author + " having " + object.pages + " pages.";
+      bookInfo.textContent =
+        object.title +
+        " by " +
+        object.author +
+        " having " +
+        object.pages +
+        " pages.";
       booksOutput.appendChild(bookInfo);
+      let deleteBtn = document.createElement("button");
+      deleteBtn.setAttribute("id", i);
+      deleteBtn.classList.add("delete");
+      deleteBtn.textContent = "Delete";
+      booksOutput.appendChild(deleteBtn);
+      deleteBtn.addEventListener("click", () => {
+        myLibrary = myLibrary.splice(deleteBtn.id, 1);
+        console.log(deleteBtn.id);
+        printBooks();
+      });
     }
   }
 };
